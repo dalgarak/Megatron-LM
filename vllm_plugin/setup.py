7e761cda8c231570dd801d3debd6b2b23166e266
cyr0930 <line0930@gmail.com>
2025-12-16 06:34:30 -0800

    fix vllm model version

--- vllm_plugin/setup.py
+++ vllm_plugin/setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 shutil.copy("../bridge/configuration_wbl.py", "./wbl/")
 setup(
     name='wbl',
-    version='1.0.1',
+    version='1.1.0',
     packages=['wbl'],
     entry_points={
         'vllm.general_plugins': [
